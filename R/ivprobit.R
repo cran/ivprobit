7ebf761085beacbb0c030587131f63894f7bd603
Zaghdoudi Taha <zedtaha@gmail.com>
2018-02-21 22:10:43 +0000

    version 1.1

--- R/ivprobit.R
+++ R/ivprobit.R
@@ -1,26 +1,46 @@
 #'Foreign-Exchange Derivatives Use By Large U.S. Bank Holding Com-panies (1996-2000).
 #'
-#'@docType data
-#'@name eco.RData
+#' @docType data
+#' @name  eco
+#' @usage data(eco)
+#' @format A data frame with 794 rows and 22 variables
 NULL
 
 
 #'Instrumental Variables Probit function
 #'
-#'@author Zaghdoudi Taha
-#'@param  y is the dichotomous l.h.s. 
-#'@param x1 is the r.h.s. exogenous variables
-#'@param  y2 is the r.h.s. endogenous variables 
-#'@param  x is the complete set of instruments
-#'@return Coefficients: a named vector of coefficients
-#'@return se: a named vector of standard error
-#'@return tval: a named vector of wald statistics
-#'@return pval: a named vector P-values
-#'@return df: the degree of freedom
+#'@param formula y~x|y1|x2 whre y is the dichotomous l.h.s.,x is the r.h.s. exogenous variables, 
+#' y1 is the r.h.s. endogenous variables and x2 is the complete set of instruments
+#'@param data the dataframe
+#'@importFrom stats lm glm binomial coef as.formula model.frame model.matrix model.response na.omit update vcov pchisq printCoefmat pt
+#'@import Formula
+#'
+#'@examples
+#'############################################
+#'# Fit the ivprobit model
+#'############################################
+#'# Load data
+#'data(eco)
+#'############################################
+#'pro<-ivprobit(d2~ltass+roe+div|eqrat+bonus|ltass+roe+div+gap+cfa,eco)
+#'summary(pro)
+#'
 #'@export
-ivprob<-function(y,x1,y2,x){
-  
-  y<-y
+ivprobit<-function(formula,data){
+  fm<-formula
+  mf <- match.call(expand.dots = FALSE)
+  m <- match(c("formula", "data", "subset", "na.action"), names(mf), 0)
+  mf <- mf[c(1, m)]
+  ffm <- Formula::Formula(fm)
+  mf[[1]] <- as.name("model.frame")
+  mf$formula <- ffm
+  fffm<-update(ffm, ~ . -1| . -1|. -1)
+  mf <- model.frame(formula=fffm, data=data)
+  x1 <- model.matrix(fffm, data = mf, rhs = 1)
+  y2 <- model.matrix(fffm, data = mf, rhs = 2)
+  x <- model.matrix(fffm, data = mf, rhs = 3)
+  y<-model.response(mf)
+  y<- as.matrix(y)
   X<-as.matrix(x)
   Y<-as.matrix(y2)
   X1<-as.matrix(x1)
@@ -36,7 +56,6 @@ ivprob<-function(y,x1,y2,x){
   yhat<-lin$fitted.values
   uhat<-lin$residuals
   #-----------------------------
-  
   d<-chol2inv(chol(t(XX)%*%XX))%*%t(XX)%*%Z
   #step2
   prob<-glm(y~X+uhat,family=binomial(link="probit"))
@@ -61,14 +80,13 @@ ivprob<-function(y,x1,y2,x){
   se = sqrt(diag(cov))
   delt = cov%*%t(d)%*%invpd(omega)%*%alpha
   df=nrow(x1)-ncol(x)
-  #res<-cbind(delt,se,delt/se,p.value = 2*pt(-abs(delt/se), df=df))
   r1<-lm(y~X1+yhat)
-  names<-rownames(coef(summary(r1)))
-  #rownames(res) <- rownames(coef(summary(r1)))
-  #colnames(res) <- c("Coef","S.E.","t-stat","p-val")
-  #res
-  list(coefficients=delt,se=se,tval=(delt/se),pval=2*pt(-abs(delt/se), df=df),df=df,names=names)
-  
+  mr1<-cbind(y,X1,yhat)
+  names<-c("Intercep",colnames(x1),colnames(y2))
+  out<-list(coefficients=delt,se=se,tval=(delt/se),pval=2*pt(-abs(delt/se), df=df),df=df,names=names,mr1=mr1)
+  class(out) <- "ivprobit"
+  # Return results.
+  return(out)
 }
 
 invpd<-function(mat){
@@ -81,32 +99,7 @@ glmvcov<-function(obj){
   so$dispersion * so$cov.unscaled
   
 }
-#' method
-#' 
-#' @author Zaghdoudi Taha
-#' @param x a numeric design matrix for the model.
-#' @param ... not used
-#' @export
-ivprobit <- function(x,...){UseMethod("ivprobit") }
 
-ivprobit.default <- function(y,x1,y2,x,....){
-  
-  est <- ivprob(y,x1,y2,x)
-  est$vcov<-est$cov
-  #est$fitted.values <- as.vector(x %*% est$coefficients)
-  #est$residuals <- y - est$fitted.values
-  est$call <- match.call()
-  class(est) <- "ivprobit"
-  est
-  
-}
-print.ivprobit <- function(x,...)
-{
-  cat("Call:\n")
-  print(x$call)
-  cat("\nCoefficients:\n")
-  print(x$coefficients)
-}
 #' Summary
 #' 
 #' @param object is the object of the function
@@ -120,27 +113,4 @@ summary.ivprobit<-function(object,...)
   colnames(res) <- c("Coef","S.E.","t-stat","p-val")
   printCoefmat(res,P.values=TRUE,has.Pvalue=TRUE)
   
-}
-#' formula
-#' 
-#' @param formula1 first formula
-#' @param formula2 second formula
-#' @param data the dataset 
-#'@param ... not used
-#'@export
-ivprobit.formula <-function(formula1,formula2,data=list(),...)
-{
-  mff <- update(formula1, ~ . -1)
-  mf1 <- model.frame(mff, data)
-  y <- model.response(mf1)
-  x1 <- model.matrix(attr(mf1, "terms"), mf1)
-  # endog
-  mff2 <- update(formula2, ~ . -1)
-  mf2 <- model.frame(mff2, data)
-  y2 <- model.response(mf2)
-  x <- model.matrix(attr(mf2, "terms"), mf2)
-  est <- ivprobit.default(y,x1,y2,x,...)
-  est$call <- match.call()
-  est$formula1 <- formula
-  est
 }
